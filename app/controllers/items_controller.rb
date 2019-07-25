752d029ffd7e2f3f4ce047a47f784df8967a59c8
Decosan <junx0401@gmail.com>
2019-07-25 15:33:22 +0900

    編集・削除作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :set_params, only:[:show,:edit,:update,:destroy]
+
   def index
     @items = Item.all
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -14,7 +15,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    binding.pry
     if @item.save
       redirect_to items_path, notice: '物件を登録しました'
     else
@@ -23,15 +23,27 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    2.times {@item.stations.build}
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to items_path(@user)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_path
   end
 
   private
+
+  def set_params
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name,:price,:age,:address,:remark,stations_attributes: [:name,:line,:distance,:item_id,:_destroy])
